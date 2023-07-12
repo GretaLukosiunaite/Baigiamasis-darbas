8504e9eca8685503a127b87b720da4ea312aa795
Greta Lukošiūnaitė <greta.lukosiunaite@outlook.com>
2023-07-12 19:57:39 +0300

    added headline row in table component

--- Client/src/components/organisims/Table/Table.tsx
+++ Client/src/components/organisims/Table/Table.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import TableRow from '../../molecules/TableRow';
+import {
+  StyledHeadlineContainer,
+  StyledHeadlineRow,
+  StyledTable,
+} from '../../molecules/TableRow/styles';
 
 const Table = () => {
   return (
-    <div>
+    <StyledTable>
+      <StyledHeadlineRow>
+        <StyledHeadlineContainer className='table-header'>
+          <h6>Name</h6>
+        </StyledHeadlineContainer>
+        <StyledHeadlineContainer className='table-header'>
+          <h6>Lastname</h6>
+        </StyledHeadlineContainer>
+        <StyledHeadlineContainer className='table-header'>
+          <h6>Email</h6>
+        </StyledHeadlineContainer>
+        <StyledHeadlineContainer className='table-header'>
+          <h6>Age</h6>
+        </StyledHeadlineContainer>
+        <StyledHeadlineContainer className='table-header'>
+          <h6></h6>
+        </StyledHeadlineContainer>
+      </StyledHeadlineRow>
       <TableRow></TableRow>
-    </div>
+    </StyledTable>
   );
 };
 
