ec501ae239e0666df4ab86bf29c16df86a91423b
Greta Lukošiūnaitė <greta.lukosiunaite@outlook.com>
2023-07-14 11:18:01 +0300

    BNtton.tsx deleted commented lines

--- Client/src/components/atoms/Button/Button.tsx
+++ Client/src/components/atoms/Button/Button.tsx
@@ -1,21 +1,14 @@
-
 interface IButtonProps {
   text: string;
   action?: () => void;
-  className: string
-  // fullwidth?: boolean | undefined;
-  // outline?: boolean | undefined;
-  // color?: COLOR;
+  className: string;
 }
 
 const Button = ({ text, action, className }: IButtonProps) => {
   return (
     <button
-    className = {className ? `button ${className}`: `button`}
+      className={className ? `button ${className}` : `button`}
       onClick={action}
-      // fullwidth={fullwidth ? 1 : 0}
-      // outline={outline ? 1 : 0}
-      // color={color}
     >
       {text}
     </button>
