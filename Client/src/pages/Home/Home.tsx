1ea30f9a58973f538ab3d2fcec8da9abdce02da8
Greta Lukošiūnaitė <greta.lukosiunaite@outlook.com>
2023-07-14 11:29:54 +0300

    Home.tsx transfered datafetching from table row and added logic to handle addedParticipant, deleteParticipant and update Participant

--- Client/src/pages/Home/Home.tsx
+++ Client/src/pages/Home/Home.tsx
@@ -1,10 +1,10 @@
-import { SetStateAction, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Table from '../../components/organisims/Table';
 import Footer from '../../components/templates/Footer';
 import Header from '../../components/templates/Header';
 import { IParticipant } from '../../shared/api/types';
 import { API } from '../../shared/api';
-import { StyledLayoutPage } from '../../components/templates/Layout/styles';
+import { StyledLayoutPage } from './styles';
 
 const Home = () => {
   const [participants, setParticipants] = useState<IParticipant[]>([]);
@@ -13,15 +13,50 @@ const Home = () => {
   useEffect(() => {
     const fetchParticipants = async () => {
       try {
-        const fetchedParticipants = await API.getParticipants();
-        setParticipants(fetchedParticipants);
+        const fetchedParticipantsData = await API.getParticipants();
+        setParticipants(fetchedParticipantsData);
       } catch (error) {
-        console.error('Error fetching participants:', error);
+        console.error('Error getting participants:', error);
       }
     };
-
     fetchParticipants();
   }, []);
+
+  const participantAddedLogic = (participant: IParticipant) => {
+    // Add the new participant to the participants list
+    setParticipants((prevParticipants) => [...prevParticipants, participant]);
+  };
+
+  const deleteParticipantLogic = async (participantId: string) => {
+    try {
+      await API.deleteParticipant(participantId);
+      // Remove the deleted participant from the participants list
+      setParticipants((prevParticipants) =>
+        prevParticipants.filter(
+          (participant) => participant._id !== participantId
+        )
+      );
+    } catch (error) {
+      console.error('Error deleting participant:', error);
+    }
+  };
+
+  const updateParticipantLogic = async (
+    participantId: string,
+    updatedData: IParticipant
+  ) => {
+    try {
+      await API.updateParticipant(participantId, updatedData);
+      // Update the participant in the participants list
+      setParticipants((prevParticipants) =>
+        prevParticipants.map((participant) =>
+          participant._id === participantId ? updatedData : participant
+        )
+      );
+    } catch (error) {
+      console.error('Error updating participant:', error);
+    }
+  };
 
   const filteredParticipants = participants.filter((participant) => {
     const searchLower = searchValue.toLowerCase();
@@ -40,11 +75,19 @@ const Home = () => {
 
   return (
     <StyledLayoutPage>
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
+      <Header
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        onParticipantAdded={participantAddedLogic}
+      />
       {filteredParticipants.length > 0 ? (
-        <Table/>
+        <Table
+          participants={filteredParticipants}
+          onDeleteParticipant={deleteParticipantLogic}
+          onUpdateParticipant={updateParticipantLogic}
+        />
       ) : (
-        <p>Nėra rezultatų</p>
+        <p className='box'>Nėra rezultatų</p>
       )}
       <Footer />
     </StyledLayoutPage>
